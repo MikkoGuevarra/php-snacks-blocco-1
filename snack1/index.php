4b5a132f4cb3470af487c726cf27b962f5ca18dc
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-12-15 15:36:17 +0100

    created array for games snack 1

---
+++ snack1/index.php
@@ -0,0 +1,38 @@
+<?php
+
+$games = [
+    'game 1' => [
+        'home' => [
+            'name' => 'lakers',
+            'score' => rand(50, 130)
+        ],
+        'away' => [
+            'name' => 'warriors',
+            'score' => rand(50, 130)
+        ]
+    ],
+    'game 2' => [
+        'home' => [
+            'name' => 'clippers',
+            'score' => rand(50, 130)
+        ],
+        'away' => [
+            'name' => 'nets',
+            'score' => rand(50, 130)
+        ]
+    ]
+
+];
+var_dump($games);
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Nba season 2020/2021</h1>
+    </body>
+</html>
